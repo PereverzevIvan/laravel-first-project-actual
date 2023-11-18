b0a79423159aabc6ceef86f01dd1b439b17819ca
PereverzevIvan <peregh320@gmail.com>
2023-11-18 23:02:31 +0300

    Add delete method for articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -87,6 +87,7 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        $article->delete();
+        return redirect('/article');
     }
 }
